b0032b69ad7df5b8b91650f8ea5635f7cfef3830
misbashaikh2004 <misba6602@gmail.com>
2025-11-05 19:03:18 +0530

    First commit

---
+++ SwappingNumbers.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class SwappingNumbers{
+   public static void main(String[] args){
+
+   int a,b;
+   Scanner sc = new Scanner(System.in);
+
+   System.out.println("Enter 2 Numbers");
+   a=sc.nextInt();
+   b=sc.nextInt();
+
+   a = a + b;
+   b = a - b;
+   a = a - b;
+  
+   System.out.println("Swapping is:" + a + b);
+   }
+}
