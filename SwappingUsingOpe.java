32eec7b842946d99feb2f9e8f75166d36b8f9a98
misbashaikh2004 <misba6602@gmail.com>
2025-11-05 19:09:42 +0530

    First commit

---
+++ SwappingUsingOpe.java
@@ -0,0 +1,21 @@
+//7. Accept two integers from the user and interchange them. Display the interchanged numbers. Using temporary variable Without using temporary 
+//variable b. Using / and * operator 
+
+import java.util.Scanner;
+public class SwappingUsingOpe{
+   public static void main(String[] args){
+
+   int a,b;
+   Scanner sc = new Scanner(System.in);
+
+   System.out.println("Enter 2 Numbers");
+   a=sc.nextInt();
+   b=sc.nextInt();
+
+   a = a / b;
+   b = a * b;
+   a = a * b;
+  
+   System.out.println("Swapping is:" + a +" " + b);
+   }
+}
